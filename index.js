9a3398886d4f7826891ecdeb2802065336b11022
Hamad <AlmarzooqiH23@gmail.com>
2026-04-16 22:50:44 +0400

    Forcing network maybe it work now T_T

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.post("/check", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Im going crazy atm T_T API running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
