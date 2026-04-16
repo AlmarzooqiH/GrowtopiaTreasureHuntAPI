e6318527107704e84057b28ab2868e26f283cb5f
Hamad <AlmarzooqiH23@gmail.com>
2026-04-16 22:34:35 +0400

    Changed env name cuz maybe it is conflicting

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.post("/check", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-console.log("LISTENING ON", PORT);
-app.listen(PORT, () => {
-  console.log(`API running on port ${PORT}`);
+const PORT = process.env.PORTT || 3000;
+console.log("LISTENING ON", PORTT);
+app.listen(PORTT, () => {
+  console.log(`API running on port ${PORTT}`);
 });
