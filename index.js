41dfd6ec47dc6bc0d07ed8c264b7cbea7692cd7e
Hamad <AlmarzooqiH23@gmail.com>
2026-04-17 00:35:07 +0400

    maybe

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const cors = require("cors");
 
 const app = express();
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    const allowed = [
-      "https://donatev2s.com",
-      "https://www.donatev2s.com"
-    ];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     const allowed = [
+//       "https://donatev2s.com",
+//       "https://www.donatev2s.com"
+//     ];
 
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-};
+//     if (!origin || allowed.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   }
+// };
 
-app.use(cors(corsOptions));app.use(express.json());
-
+// app.use(cors(corsOptions));app.use(express.json());
+app.use(
+  cors({
+      origin: "*",
+    })
+)
 console.log("Setup completed");
 
 app.post("/check", (req, res) => {
